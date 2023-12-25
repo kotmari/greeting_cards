ed80b31af3f12ae5d6d624167a111ef6f1b469c5
Пользователь <mariannakot79@gmail.com>
2023-12-25 13:39:22 +0200

    Перший коміт: Опис змін

---
+++ app.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+   effect: "cards",
+   grabCursor: true,
+   initialSlide: 2,
+   loop: true,
+   rotate: true,
+   mousewheel: {
+      invert: false,
+   }
+
+})
